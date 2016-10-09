561d8b1b07a3f09e3832ecac4cd194843f4aa980
yujichino <au4iu6@gmail.com>
2016-10-09 08:54:53 +0000

    add timer

--- shoot2016.py
+++ shoot2016.py
@@ -3,12 +3,16 @@
 from scene import *
 import sound
 import random
+import time
 from math import sin, cos, pi
 
 A = Action
 
 standing_texture = Texture('plf:AlienGreen_front')
 walk_textures = [Texture('plf:AlienGreen_walk1'), Texture('plf:AlienGreen_walk2')]
+
+game_duration = 10
+disp_end_duration = 5
 
 # 障害物
 class Meteor (SpriteNode):
@@ -30,10 +34,16 @@ class GameScene (Scene):
 		self.add_child(self.player)
 		
 		# スコアを配置
-		score_front = ('Futura', 40)
-		self.score_label = LabelNode('0', score_front, parent=self)
+		score_font = ('Futura', 40)
+		self.score_label = LabelNode('0', score_font, parent=self)
 		self.score_label.position = (self.size.w/2, self.size.h - 70)
 		self.score_label.z_position = 5
+		
+		# 制限時間を配置
+		time_font = ('Futura', 40)
+		self.time_label = LabelNode('00:00', time_font, parent=self)
+		self.time_label.position = (self.size.w/3, self.size.h - 70)
+		self.time_label.z_position = 5
 
 		self.items = []
 		self.lasers = []				
@@ -53,6 +63,8 @@ class GameScene (Scene):
 		self.score = 0
 		self.score_label.text = '0'
 		
+		self.start_time = time.time()
+		
 		self.walk_step = -1
 		
 		self.player.position = (self.size.w/2, 32)
@@ -60,21 +72,35 @@ class GameScene (Scene):
 		self.speed = 1.0
 		
 		self.game_over = False
-		
+		self.disp_end_time = disp_end_duration
+				
 		self.spawn_item()
 	
 	def update(self):
 		if self.game_over:
+			# ゲーム完了後、一定時間は完了画面を表示する
+			time_passed = time.time() - self.disp_start_time
+			self.disp_end_time = max(0, int(disp_end_duration - time_passed))
+			self.time_label.text = '{0}:{1:0>2}'.format(self.disp_end_time/60, self.disp_end_time%60)
 			return
+
 		# プレイヤーの動きを更新
 		self.update_player()
 		# レーザーと障害物の衝突チェック
 		self.check_laser_collisions()
+		# 制限時間を更新
+		time_passed = time.time() - self.start_time
+		t = max(0, int(game_duration - time_passed))
+		self.time_label.text = '{0}:{1:0>2}'.format(t/60, t%60)
+		# タイムアップかチェックする
+		if t == 0 and not self.game_over:
+			self.disp_end_game('NG')
 		
 	def touch_began(self, touch):
 		if self.game_over:
-			self.end_label.remove_from_parent()
-			self.new_game()
+			if self.disp_end_time == 0:
+				self.end_label.remove_from_parent()
+				self.new_game()
 		else:
 			self.shoot_laser()
 	
@@ -133,21 +159,33 @@ class GameScene (Scene):
 			
 	def check_end_game(self):
 		if len(self.items) == 0:
-			self.disp_end_game()
+			self.disp_end_game('OK')
 	
-	def disp_end_game(self):
-		end_front = ('Arial Rounded MT Bold', 80)
-		self.end_label = LabelNode('End Game', end_front, parent=self)
+	def disp_end_game(self, status):
+		
+		if status == 'OK':
+			message = 'Game Clear'
+			color = '#00ff00'
+			background_color = '#ffff00'
+			effect = 'arcade:Powerup_1'
+		else:
+			message = 'Time Up'
+			color = '#ff0000'
+			background_color = '#000000'
+			effect = 'arcade:Explosion_1'
+		
+		end_font = ('Arial Rounded MT Bold', 80)
+		self.end_label = LabelNode(message, end_font, parent=self)
 		self.end_label.position = (self.size.w/2, self.size.h - 300)
 		self.end_label.z_position = 1
-		self.end_label.color = '#ff0000'
+		self.end_label.color = color
+		self.background_color = background_color
 		
-		self.background_color = '#f4ff00'
-	
 		self.game_over = True
+		self.disp_start_time = time.time()
 		
-		sound.play_effect('arcade:Explosion_1')
-	
+		sound.play_effect(effect)
+			
 	def spawn_item(self):
 		for i in range(1):
 			for j in range(9):
